26ce72c1f613414be3be2346a59eb49ca0876c09
Andrew Leung <andrewleung104@gmail.com>
2023-10-17 20:40:28 -0400

    Anything mango with cheese foam

--- src/main.go
+++ src/main.go
@@ -7,8 +7,49 @@ import (
 	"os"
 	"os/signal"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/urfave/cli/v2"
 )
+
+const TARGET_STRING = "kartik josh aaron"
+
+type model struct {
+	targetStringIndex int
+}
+
+func initialModel() model {
+	return model{
+		targetStringIndex: 0,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		default:
+			if m.targetStringIndex >= len(TARGET_STRING) {
+				return m, nil
+			}
+
+			if msg.String() == string(TARGET_STRING[m.targetStringIndex]) {
+				m.targetStringIndex++
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return TARGET_STRING[:m.targetStringIndex]
+}
 
 func main() {
 	_, rootCancel := signal.NotifyContext(context.Background(), os.Interrupt)
@@ -52,7 +93,12 @@ func startTest() *cli.Command {
 		Usage: "Start typing test",
 		Flags: []cli.Flag{lengthFlag},
 		Action: func(ctx *cli.Context) error {
-			fmt.Printf("This is a test: %v", length)
+			p := tea.NewProgram(initialModel())
+			if _, err := p.Run(); err != nil {
+				fmt.Printf("Alas, there's been an error: %v", err)
+				os.Exit(1)
+			}
+
 			return nil
 		},
 	}
